06765813239b85e0237e36e2022ded9309621afb
Dmitry Plotnikov <dmitplotn@gmail.com>
2021-02-28 10:12:31 +0500

    remove response sender

--- ex1/check.php
+++ ex1/check.php
@@ -1,27 +1,37 @@
 <?php
 
 require_once __DIR__ . '/LeapYearChecker.php';
-require_once __DIR__ . '/ResponseSender.php';
 
 use function Ex1\LeapYearChecker\isLeapYear;
-use function Ex1\ResponseSender\send;
+
+header("Content-Type: application/json; charset=UTF-8");
 
 if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-    send('Invalid HTTP method.', 404);
+    http_response_code(404);
+    echo json_encode(['message' => "Неверный HTTP метод"]);
+    exit;
 }
 
 if (!isset($_GET['year'])) {
-    send("Required HTTP parameter('year') is not defined.", 404);
+    http_response_code(404);
+    echo json_encode(['message' => "Требуемый HTTP параметр отсутсвует в запросе"]);
+    exit;
 }
 
 $year = $_GET['year'];
 
 if (!filter_var($year, FILTER_VALIDATE_INT)) {
-    send('ОШИБКА ВО ВХОДНЫХ ДАННЫХ', 422);
+    http_response_code(422);
+    echo json_encode(['message' => 'ОШИБКА ВО ВХОДНЫХ ДАННЫХ']);
+    exit;
 }
 
 try {
-    send(isLeapYear((int) $year) ? 'ДА' : 'НЕТ');
+    $message = isLeapYear((int) $year) ? 'ДА' : 'НЕТ';
+    echo json_encode(['message' => $message]);
+    exit;
 } catch (\LogicException $e) {
-    send('ОШИБКА ВО ВХОДНЫХ ДАННЫХ', 422);
+    http_response_code(422);
+    echo json_encode(['message' => 'ОШИБКА ВО ВХОДНЫХ ДАННЫХ']);
+    exit;
 }
